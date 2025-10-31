b1697f1ff4b8952f34af460ab2617ed943f426ed
Gustavo Maciel <gustavomsilva233@gmail.com>
2025-10-31 09:11:35 -0300

    Aula de SWITCH

--- src/estudojava/introducao/Aula03EstruturasCondicionais.java
+++ src/estudojava/introducao/Aula03EstruturasCondicionais.java
@@ -27,5 +27,39 @@ public class Aula03EstruturasCondicionais {
         Embora não seja aconselhável podemos usar um operador ternario para simplificar uma estrutura if else.
         OPERADOR TERNÁRIO: (condição) ? verdadeiro : falso
          */
+
+        /*
+        Outra estrutura de dados é o SWITCH.
+        O switch é uma estrutura condicional para resolver problemas mais simples como por exemplo,
+        imprimir o dia da semana de forma numérica.
+         */
+        byte dia = 1;
+        switch (dia){
+            case 1:
+                System.out.println("Domingo");
+                break;
+                case 2:
+                System.out.println("Segunda");
+                break;
+                case 3:
+                System.out.println("Terça");
+                break;
+                case 4:
+                System.out.println("Quarta");
+                break;
+                case 5:
+                System.out.println("Quinta");
+                break;
+                case 6:
+                System.out.println("Sexta");
+                break;
+                case 7:
+                System.out.println("Sabado");
+                break;
+            default:
+                System.out.println("Opção inválida");
+                break;
+        }
+
     }
 }
