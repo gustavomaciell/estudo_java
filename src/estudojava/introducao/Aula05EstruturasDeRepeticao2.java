9bcd558c135ddc8768f9e414420235c3e347b99f
Gustavo Maciel <gustavomsilva233@gmail.com>
2025-11-01 10:39:12 -0300

    Aula05EstruturasDeRepeticao2

---
+++ src/estudojava/introducao/Aula05EstruturasDeRepeticao2.java
@@ -0,0 +1,15 @@
+package estudojava.introducao;
+
+public class Aula05EstruturasDeRepeticao2 {
+    public static void main(String[] args) {
+        //Imprima os 25 primeiros números de um dado valor. Por exemplo, 50.
+
+        for (int i = 0; i <= 50; i++) {
+            if (i > 25){
+                break;
+            }
+            System.out.println(i);
+
+        }
+    }
+}
